06e3d43ec6abeee686f8c43b787c549efde9574c
DevNub <ddac_dan@hotmail.com>
2018-10-23 03:43:12 -0500

    Ex. 2

--- maze.js
+++ maze.js
@@ -1,24 +1,21 @@
 var hitBoundary1;
 var hitBoundary;
 
-window.onload = function(){ //Ready State
+window.onload = function(){
 
   hitBoundary1 = document.getElementById("boundary1");
-  hitBoundary = document.getElementsByClassName("boundary");
+  hitBoundary = document.querySelectorAll(".boundary");
   
-  hitBoundary1.onmouseover = function() {loss}
-  hitBoundary[0].onmouseover = function() {iloss(0)}
-  hitBoundary[1].onmouseover = function() {iloss(1)}
-  hitBoundary[2].onmouseover = function() {iloss(2)}
-  hitBoundary[3].onmouseover = function() {iloss(3)}
-  hitBoundary[4].onmouseover = function() {iloss(4)}
-  
-   function loss(){
-    hitBoundary1.className +=' youlose';
+
+  for(var i=0; i<hitBoundary.length-1; i++){
+   hitBoundary[i].onmouseover=iloss;
   }
 
-  function iloss(index){
-    hitBoundary[index].className+=' youlose';
+ function iloss(){
+  for(i=0; i<hitBoundary.length-1; i++){
+     hitBoundary[i].className+=' youlose';
+    }
   }
-
+  
 }
+
