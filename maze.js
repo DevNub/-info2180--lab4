b48340134f03398fabbd8ad0e4ad16145fa70069
DevNub <ddac_dan@hotmail.com>
2018-10-23 04:16:07 -0500

    Ex. 3

--- maze.js
+++ maze.js
@@ -1,20 +1,41 @@
-var hitBoundary1;
+var start;
+var end;
+var begin;
 var hitBoundary;
 
 window.onload = function(){
 
-  hitBoundary1 = document.getElementById("boundary1");
+  start = document.getElementById("start");
+  end = document.getElementById("end");
+  begin  = false;
   hitBoundary = document.querySelectorAll(".boundary");
   
 
-  for(var i=0; i<hitBoundary.length-1; i++){
-   hitBoundary[i].onmouseover=iloss;
+
+  start.onmouseover = function(){
+    begin = true;
+    for(var i=0; i<hitBoundary.length-1; i++){
+      hitBoundary[i].onmouseover=iloss;
+    }
   }
+
+  end.onmouseover = function(){
+    if(begin) won();
+  }
+ 
+
+
+ function won(){
+   alert("You won!");
+   gameStart = false;
+ }
+
 
  function iloss(){
   for(i=0; i<hitBoundary.length-1; i++){
      hitBoundary[i].className+=' youlose';
     }
+   console.log('Sorry Try agiain');
   }
   
 }
